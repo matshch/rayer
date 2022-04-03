de11b9e8f41a7fb577e3da8636b4765281cca9ef
Artem Leshchev <matshch@gmail.com>
2022-04-03 21:08:43 +0300

    Implement Shlick approximation

--- dielectric.go
+++ dielectric.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"math"
+	"math/rand"
+)
 
 type Dielectric struct {
 	RefractionIndex float64
@@ -16,10 +19,17 @@ func (d Dielectric) Scatter(r Ray, hit Hit) (*Ray, *Color) {
 	sinTheta := math.Sqrt(1 - cosTheta*cosTheta)
 
 	var direction Vector
-	if cannotRefract := refractionRatio*sinTheta > 1; cannotRefract {
+	cannotRefract := refractionRatio*sinTheta > 1
+	if cannotRefract || reflectance(cosTheta, refractionRatio) > rand.Float64() {
 		direction = directionUnit.Reflect(hit.Normal)
 	} else {
 		direction = directionUnit.Refract(hit.Normal, refractionRatio)
 	}
 	return &Ray{hit.Point, direction}, &Color{R: 1, G: 1, B: 1}
+}
+
+func reflectance(cos, refractionRatio float64) float64 {
+	r0 := (1 - refractionRatio) / (1 + refractionRatio)
+	r0 = r0 * r0
+	return r0 + (1-r0)*math.Pow((1-cos), 5)
 }
