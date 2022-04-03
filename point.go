e133731a97572ec1105afa291ad6a0f7d33ef288
Artem Leshchev <matshch@gmail.com>
2022-04-03 14:09:14 +0300

    Implement simple vector and point operations

--- point.go
+++ point.go
@@ -11,3 +11,7 @@ type Point struct {
 func (p Point) String() string {
 	return fmt.Sprintf("(%g, %g, %g)", p.X, p.Y, p.Z)
 }
+
+func (p Point) Add(v Vector) Point {
+	return Point{X: p.X + v.X, Y: p.Y + v.Y, Z: p.Z + v.Z}
+}
--- vector.go
+++ vector.go
@@ -11,3 +11,15 @@ type Vector struct {
 func (v Vector) String() string {
 	return fmt.Sprintf("<%g, %g, %g>", v.X, v.Y, v.Z)
 }
+
+func (v Vector) Scale(t float64) Vector {
+	return Vector{X: t * v.X, Y: t * v.Y, Z: t * v.Z}
+}
+
+func (a Vector) Add(b Vector) Vector {
+	return Vector{X: a.X + b.X, Y: a.Y + b.Y, Z: a.Z + b.Z}
+}
+
+func (a Vector) Sub(b Vector) Vector {
+	return Vector{X: a.X - b.X, Y: a.Y - b.Y, Z: a.Z - b.Z}
+}
